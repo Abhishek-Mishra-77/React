56ae3318ba587c1521bbdd50134ef71845dac6ea
Abhishek777777777 <abhishekmishra992016@gmail.com>
2023-05-31 19:01:31 +0530

    Traverse Item in loop

--- src/App.js
+++ src/App.js
@@ -15,58 +15,38 @@ function App() {
       title: 'Toilet Paper',
       LocationOfExpenditure: 'Health',
       amount: 100.85,
-      date:new Date(2023, 5, 28),
+      date: new Date(2023, 5, 28),
     },
     {
       id: 'e3',
       title: 'Groceries',
       LocationOfExpenditure: 'Food',
       amount: 2025.6,
-      date:new Date(2023, 5, 26),
+      date: new Date(2023, 5, 26),
     },
     {
       id: 'e4',
       title: 'Movies',
       LocationOfExpenditure: 'Enjoy',
       amount: 1000.23,
-      date:new Date(2023, 5, 15),
+      date: new Date(2023, 5, 15),
     },
   ];
 
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        LocationOfExpenditure={expenses[0].LocationOfExpenditure}
-        date={expenses[0].date}>
-
-      </ExpenseItem>
-
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        LocationOfExpenditure={expenses[1].LocationOfExpenditure}
-        date={expenses[1].date}>
-
-      </ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        LocationOfExpenditure={expenses[2].LocationOfExpenditure}
-        date={expenses[2].date}>
-
-      </ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        LocationOfExpenditure={expenses[3].LocationOfExpenditure}
-        date={expenses[3].date}>
-
-      </ExpenseItem>
-    </div >
+      {expenses.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          LocationOfExpenditure={expense.LocationOfExpenditure}
+          date={expense.date}
+        />
+      ))}
+    </div>
   )
+
 }
 
 export default App;
