0d6703e01ab1dde50b0a78087d2e33ff576825a6
Abhishek777777777 <abhishekmishra992016@gmail.com>
2023-05-31 15:44:31 +0530

    Props Manually

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,16 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-    const expenseDate = new Date(2023,5,31)
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.77;
-    const LocationOfExpenditure = 'Uttar Pradesh'
-    return (<div className='expense-item'>
-        <div>{expenseDate.toISOString()}</div>
-        <div className='expense-item_description'>
-            <h2>{expenseTitle}</h2>
-            <h2>{LocationOfExpenditure}</h2>
-            <div className='expense-item_price'>${expenseAmount}</div>
-            </div>  
-                   
-    </div>)
+function ExpenseItem(props) {
+    return (
+        <div className='expense-item'>
+            <div>{props.date.toISOString()}</div>
+            <div className='expense-item_description'>
+                <h2>{props.title}</h2>
+                <h2>{props.LocationOfExpenditure}</h2>
+                <div className='expense-item_price'>${props.amount}</div>
+            </div>
+
+        </div>)
 }
 
 
