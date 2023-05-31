b7b7098928650937799f93d49fc81e53ec386760
Abhishek777777777 <abhishekmishra992016@gmail.com>
2023-05-31 20:50:24 +0530

    addDateInExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,17 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+     const month = props.date.toLocaleString('en-US',{month:'long'});
+     const day = props.date.toLocaleString('en-US', {day: '2-digit'})
+     const year = props.date.getFullYear();
+
     return (
         <div className='expense-item'>
-            <div>{props.date.toISOString()}</div>
+            <div className='date'>
+            <div>{day}</div>    
+            <div>{month}</div>    
+            <div>{year}</div>    
+            </div>
             <div className='expense-item_description'>
                 <h2>{props.title}</h2>
                 <h2>{props.LocationOfExpenditure}</h2>
@@ -12,7 +20,6 @@ function ExpenseItem(props) {
 
         </div>)
 }
-
 
 
 export default ExpenseItem;
