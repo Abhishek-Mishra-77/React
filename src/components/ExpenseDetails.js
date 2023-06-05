e082450c7987e1acc0e49b45f95efcb109e74824
Abhishek777777777 <abhishekmishra992016@gmail.com>
2023-06-05 20:02:14 +0530

    secondComponentGetAddded

--- src/components/ExpenseDetails.js
+++ src/components/ExpenseDetails.js
@@ -1,9 +1,12 @@
+import './ExpenseItem.css';
+
 function ExpenseDetails(props) {
     return (
         <div className="expense-item_description">
-            <div>{props.title}</div>
-            <div>{props.amount}</div>
-            <div>{props.LocationOfExpenditure}</div>
+            <div className="expense-item_description_title">{props.title}</div>
+            <div className="expense-item_description_location">{props.LocationOfExpenditure}</div>
+            <div className="expense-item_description_amount">{props.amount}</div>
+            
         </div>
     )
 }
